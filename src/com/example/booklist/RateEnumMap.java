24d9024dbb82738f225d85f23da026f3daee05ce
sh0hei <vividbit@gmail.com>
2014-03-12 20:02:30 +0900

    Daily commit

--- src/com/example/booklist/RateEnumMap.java
+++ src/com/example/booklist/RateEnumMap.java
@@ -5,7 +5,7 @@ import java.util.EnumMap;
 public class RateEnumMap {
 
 	private enum Rate {
-		NOT_SPECIFIED, SINGLE, DOUBLE, TRIPLE, QUADRUPLE, QUINTUPLE
+		NOT_SPECIFIED, SINGLE_STAR, DOUBLE_STAR, TRIPLE_STAR, QUADRUPLE_STAR, QUINTUPLE_STAR
 	}
 
 	private EnumMap<Rate, String> rateMap;
@@ -13,11 +13,11 @@ public class RateEnumMap {
 	public RateEnumMap() {
 		rateMap = new EnumMap<Rate, String>(Rate.class);
 		rateMap.put(Rate.NOT_SPECIFIED, "指定無し");
-		rateMap.put(Rate.SINGLE, "☆");
-		rateMap.put(Rate.DOUBLE, "☆☆");
-		rateMap.put(Rate.TRIPLE, "☆☆☆");
-		rateMap.put(Rate.QUADRUPLE, "☆☆☆☆");
-		rateMap.put(Rate.QUINTUPLE, "☆☆☆☆☆");
+		rateMap.put(Rate.SINGLE_STAR, "☆");
+		rateMap.put(Rate.DOUBLE_STAR, "☆☆");
+		rateMap.put(Rate.TRIPLE_STAR, "☆☆☆");
+		rateMap.put(Rate.QUADRUPLE_STAR, "☆☆☆☆");
+		rateMap.put(Rate.QUINTUPLE_STAR, "☆☆☆☆☆");
 
 	}
 }
